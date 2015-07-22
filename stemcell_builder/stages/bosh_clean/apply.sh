b01478a2546358000cc695ccf07044c12466d8ce
Pedro Alvarez <pedro.alvarez@codethink.co.uk>
2015-07-22 14:11:30 +0000

    stemcell_builder: check if file exists before writing to it

--- stemcell_builder/stages/bosh_clean/apply.sh
+++ stemcell_builder/stages/bosh_clean/apply.sh
@@ -13,7 +13,9 @@ rm -vrf $chroot/tmp/*
 
 # ubuntu trusty+ needs /etc/resolv.conf to be a symlink, so delete contents
 # instead of removing the file to preserve the link
-cat /dev/null > $chroot/etc/resolv.conf
+if [ -f $chroot/etc/resolv.conf ]; then
+  cat /dev/null > $chroot/etc/resolv.conf
+fi
 
 
 # Ubuntu Trusty resolvconf package populates /etc/resolv.conf from files below
